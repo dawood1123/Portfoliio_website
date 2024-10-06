8a308b4169f33ed4bf3a81797a5270addda6b6fe
dawood1123 <davidkhan1226@gmail.com>
2024-10-06 01:35:43 -0700

    added

---
+++ script.js
@@ -0,0 +1,9 @@
+function showhamburger(){ 
+    var hamburger = document.querySelector('.hamburger'); 
+    hamburger.style.display = 'flex'
+}
+    
+function closehamburger(){ 
+    var hamburger = document.querySelector('.hamburger'); 
+    hamburger.style.display = 'none'
+}
